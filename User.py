d391392f78dc238ef94f255448535347ed8c461b
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-01-22 18:09:31 +0300

    create four instance variables, that take up the firstname, lastname, password, and email of  new user.

--- User.py
+++ User.py
@@ -2,3 +2,16 @@ class User:
     """
     Class that generates new instances of User
     """
+listOfUser=[]
+
+def __init__(self,first_name,last_name,password,email):
+    
+        '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            first_name: New user first name.
+            last_name : New user last name.
+            password : New user password.
+            email : New user email address.
+        '''
