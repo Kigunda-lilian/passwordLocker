d31d2858df3febae80a7e6e4cfb3d86f3b3eae22
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-01-23 17:03:54 +0300

    create a test class that defines test case for the user class

--- User-test.py
+++ User-test.py
@@ -1,2 +1,6 @@
 import unittest # Importing the unittest module
 from User import User # Importing the User class
+class TestUser(unittest.TestCase):
+    '''
+    Test class that defines test case for the user class behaviour.
+    '''
