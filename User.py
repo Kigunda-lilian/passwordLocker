d641adc3b3ffc3d720f5db6e0bef1ffa310a71f0
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-01-25 21:59:02 +0300

    correct errors

--- User.py
+++ User.py
@@ -2,10 +2,11 @@ class User:
     """
     Class that generates new instances of User
     """
-listOfUser=[]
-
-def __init__(self,first_name,last_name,password,email):
+    listOfUser=[]
     
+    def __init__(self,first_name,last_name,password,email):
+        
+        
         '''
         __init__ method that helps us define properties for our objects.
 
@@ -19,55 +20,54 @@ def __init__(self,first_name,last_name,password,email):
         self.last_name = last_name
         self.password = password
         self.email = email
-        
-def save_User(self):
-            '''
-            function to add a new user to the  listofUser array
-            '''
-            User.listOfUser.append(self)  
             
-def delete_User(self):
+    def save_User(self):
+        
+        '''
+        function to add a new user to the  listofUser array
+        '''
+        User.listOfUser.append(self)  
+                
+    def delete_User(self):
         '''
         method deletes a saved user from the user list.
         '''
-
         User.listOfUser.remove(self)
-        
-        
-        @classmethod
-        def find_by_first_name(cls, first_name):
-          '''
+            
+            
+    @classmethod
+    def find_by_first_name(cls, first_name):
+        '''
         method that takes in a name and returns a firstname that matches that name.
         Args:
-            firstname:name to search for
+        firstname:name to search for
         Returns:
-            username of person that matches the firstname.
+        username of person that matches the firstname.
         '''
         for User in cls.listOfUser:
             if User.first_name == first_name :
                 return User
             
             
-        @classmethod
-        def User_exists(cls,  first_name):
-         '''
+    @classmethod
+    def User_exists(cls,  first_name):
+        
+        '''
         method that checks if a user exists from the user list.
         Args:
-            first_name: first_name to search the account exists.
+        first_name: first_name to search the account exists.
         Returns:
-            Boolean: true or false depending if the user exists.
+        Boolean: true or false depending if the user exists.
         '''
         for User in cls.listOfUser:
             if User.first_name == first_name:
                 return True
+            
 
-        return False
-    
+            return False
+
         @classmethod
         def display_Users(cls):
-         '''
-        method that returns the user list
-        '''
-        return cls.listOfUser
+             return cls.listOfUser
         
-        
+    
--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3.9
-
-import email
 from User import User
-# from credentials import Credential
+from Credentials import Credential
+
 
 def create_User(first_name,last_name, password, email):
     '''
@@ -15,28 +14,22 @@ def save_User(User):
     '''
     function to save user
     '''
-    User.save_User(User)
+    User.save_User()
 
 def delete_User(User):
     '''
     function to delete a user
     '''
-    User.delete_User(User)
+    User.delete_User()
     
 def find_User(first_name):
-        '''
-    method that takes in the firstname and returns a user that matches that name.
-    '''
-        return User.find_by_first_name(first_name)
+    return User.find_by_first_name(first_name)
     
 def check_existing_User(email):
-        '''
-    method that checks if a user exists from the  listOfUsers.
-    '''
-        return User.User_exists(email)   
+
+     return User.User_exists(email)   
     
 def create_user_details(profile, password):
-    
     details = Credential( profile, password)
     return details
 
@@ -51,11 +44,11 @@ def display_all_users():
     function to display all users
     '''
     return User.display_profile()
-
+print("hello")
 def main():
     print("Hey welcome to Password Locker App.What is your name?")
     first_name = input()
-    print(f"Hey {first_name}. what would you like to do :)")
+    print("Hey. what would you like to do? ")
     print("\n")
     
     while True:
@@ -64,59 +57,84 @@ def main():
             
             decision = int(input())
             if decision == 1:
-                 print(" Create a new Contact")
-                 print("-"*15)
+                
+                print(" Create a new Contact")
+                print("-"*15)
 
-                 print ("First name ....")
-                 first_name = input()
+                print ("First name ....")
+                first_name = input()
 
-                 print("Last name ...")
-                 last_name = input()
+                print("Last name ...")
+                last_name = input()
 
-                 print("Enter password ...")
-                 password = input()
+                print("Enter password ...")
+                password = input()
 
-                 print("Email address ...")
-                 email = input()
+                print("Email address ...")
+                email = input()
 
 
-                 save_User(create_User(first_name,last_name,password,email)) # create and save new contact.
-                 print ('\n')
-                 print(f"{first_name}, your account has been successfully created.")
-                 print ('\n')
+                save_User(create_User(first_name,last_name,password,email)) # create and save new contact.
+                print ('\n')
+                print(" your account has been successfully created.")
+                print ('\n')
                  
             elif decision == 2:
-    
                 if display_all_users():
-                   print("Here is a list of all your contacts")
-                   print('\n')
+                    print("Here is a list of all your contacts")
+                    print('\n')
 
-                   for User in display_all_users():
+                    for User in display_all_users():
                          print(f"{User.first_name} {User.last_name} .....{User.email}")
 
                          print('\n')
-            else:
-                print('\n')
-                print("You dont seem to have any contacts saved yet")
-                print('\n')
-                
+                else:
+                    print('\n')
+                    print("You dont seem to have any contacts saved yet")
+                    print('\n')
+                    
+                    
+                    
+                    
+                    
+            
+            
             elif decision == 3:
                 print("Enter your firstname")
                 first_name = input()
                 print("Enter your password")
                 password = input()
                 print("\n")
-                
+                    
                 account = User(email)
 
-            if account:
-                print(f"Hi {email}, welcome back to password locker.")
-                print("\n")
-            
-                
-                
+                if account:
+                    print("Hi , welcome back to password locker.")
+                    print("\n")
+                    
+                else:
+                    if display_all_users():
+                        print("Here is a list of all your contacts")
+                        print('\n')
+
+                    for User in display_all_users():
+                         print(f"{User.first_name} {User.last_name} .....{User.email}")
+
+                         print('\n')
+                    else:
+                        print('\n')
+                        print("You dont seem to have any contacts saved yet")
+                        print('\n')    
+           
             elif decision == 4:
-                print("Bye .......")
-                
+                        print("\nBye .......")
+                        break
             else:
-                print("I really didn't get that. Please use the short codes")    
+                        print("\nI really didn't get that. Please use the short codes")                
+            
+                    
+                 
+           
+            
+
+main()
